0df48ab0437f38357762cb44bf0c20c23aaff37f
ismael <ismaelulrich121@gmail.com>
2026-09-18 02:48:51 +0100

    external browser problem resolution v1.0

---
+++ git-init-report.md
@@ -0,0 +1,10 @@
+# swecom_customer_support_center"
+
+git init
+
+git add README.md
+
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Takam-Ismael/swecom_customer_support_center.git
+git push -u origin main
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,14 +10,13 @@ export default defineConfig({
     tailwindcss()
   ],
   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
     tsconfigPaths: true
   },
   server: {
+    host: true,
     port: 5173,
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 5173,
-    },
   },
 })
